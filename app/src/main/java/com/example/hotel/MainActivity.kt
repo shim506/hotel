a3c840294b9a1aa278a13bf9a8d51e7e1a40e5e5
shim506 <shim506@naver.com>
2022-07-14 16:51:18 +0900

    feat : navigation component 적용

--- app/src/main/java/com/example/hotel/MainActivity.kt
+++ app/src/main/java/com/example/hotel/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.hotel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.hotel.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigation,
+            findNavController(R.id.nav_host)
+        )
+
+        binding.bottomNavigation.setupWithNavController(findNavController(R.id.nav_host))
     }
+
 }
